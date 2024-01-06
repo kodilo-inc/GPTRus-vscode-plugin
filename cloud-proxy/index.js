55f933df40356033dfc394a9435331ba8ff75e53
novitckas <novitckas@selectel.ru>
2024-01-07 00:41:22 +0300

    refactor(cloud-proxy): Add passing user data

--- cloud-proxy/index.js
+++ cloud-proxy/index.js
@@ -1,36 +1,9 @@
-module.exports.handler = async function (event, context) {
-    const newPost = {
-        modelUri: 'gpt://b1g1tt95jarp1kbk20vf/yandexgpt-lite',
-        completionOptions: {
-            stream: false,
-            temperature: 0.6,
-            maxTokens: '2000',
-        },
-        messages: [
-            {
-                role: 'system',
-                text: 'Ты умный ассистент',
-            },
-            {
-                role: 'user',
-                text: 'Привет! Как мне подготовиться к экзаменам?',
-            },
-            {
-                role: 'assistant',
-                text: 'Привет! По каким предметам?',
-            },
-            {
-                role: 'user',
-                text: 'Математике и физике',
-            },
-        ],
-    };
-
+module.exports.handler = async function (event) {
     const resp = await fetch(
         'https://llm.api.cloud.yandex.net/foundationModels/v1/completion',
         {
             method: 'POST', // Здесь так же могут быть GET, PUT, DELETE
-            body: JSON.stringify(newPost), // Тело запроса в JSON-формате
+            body: event.body, // Тело запроса в JSON-формате
             headers: {
                 // Добавляем необходимые заголовки
                 'content-type': 'application/json',
